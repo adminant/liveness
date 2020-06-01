b37eac0a9d3cc0d50ff18034800e4ae0e9a6bfde
ant <a.tokarev@iteco.ru>
2020-06-01 11:52:20 +0300

    moved class from main to cameras

---
+++ lib/imghashpage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:liveness_rtmp/camerapage.dart';
+
+class ImgHashPage extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  ImgHashPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liveness App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Liveness App'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(30),
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Please enter img_hash from your task description here',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.deepPurple,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Enter img_hash here',
+                          border: OutlineInputBorder(),
+                        ),
+                        textAlign: TextAlign.center,
+                        validator: (text) {
+                          //if (text == null || text.isEmpty) {
+                          //  return 'IMG_HASH is empty!';
+                          //}
+                          return null;
+                        },
+                        onSaved: (val) {
+                          //appData.img_hash = val;
+                        },
+                        //autofocus: true,
+                      ),
+                      RaisedButton(
+                        onPressed: () {
+                          if (_formKey.currentState.validate()) {
+                            _formKey.currentState.save();
+                            Navigator.push(
+                              context,
+                              //MaterialPageRoute(builder: (context) => HomePage.withModel(cameras, ssd)),
+                              MaterialPageRoute(builder: (context) => CameraApp()),
+                            );
+                          }
+                        },
+                        child: Text('Submit'),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/services/background.dart
+++ lib/services/background.dart
@@ -3,9 +3,12 @@
 import 'package:liveness_rtmp/models/bbox.dart';
 import 'package:liveness_rtmp/services/network.dart';
 
+import 'package:camera_with_rtmp/camera.dart';
+
 class Background {
 
   Network _network;
+  List<CameraDescription> _cameras = [];
 
   bool _isSetup = false;
   bool get isConfigured => _isSetup;
@@ -22,18 +25,23 @@ class Background {
     }
   }
 
-  static Future<void> configure(
-      Network network) async {
+  static Future<void> configure(Network network, List<CameraDescription> cameras) async {
     final Background b = Background.getInstance();
 
     b._network = network;
     await b._network.init();
+
+    b._cameras = cameras;
 
     b._isSetup = true;
   }
 
   Future <BBox> getBBox() async {
     return _network.getBBox();
+  }
+
+  List<CameraDescription> getCameras() {
+    return _cameras;
   }
 
   Future<void> logout() async {
